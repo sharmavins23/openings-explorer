7fd72b3e3025cef5a9353cd643d500e3dbbbea81
sharmavins23 <sharmavins23@gmail.com>
2023-08-11 17:36:00 -0400

    Added project boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "./ThemeProvider";
+
+function App() {
+    return (
+        <ThemeProvider>
+            <h1>Hello, world!</h1>
+        </ThemeProvider>
+    );
+}
+
+export default App;
