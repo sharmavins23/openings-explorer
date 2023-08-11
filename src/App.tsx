e231c257e3d74ad7a6b2af4d5d42acdf0b50aac1
sharmavins23 <sharmavins23@gmail.com>
2023-08-11 18:43:17 -0400

    So much boilerplate later

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { ThemeProvider } from "./ThemeProvider";
+import { Divider, Stack } from "@mui/material";
 
 function App() {
     return (
-        <ThemeProvider>
-            <h1>Hello, world!</h1>
-        </ThemeProvider>
+        <>
+            <Stack
+                direction="row"
+                divider={<Divider orientation="vertical" flexItem />}
+                spacing={2}
+            >
+                <h1>Hello, world!</h1>
+                <h1>This is a test.</h1>
+            </Stack>
+        </>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App.tsx";
+import theme from "./theme.tsx";
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className="container">
+                <App />
+            </div>
+        </ThemeProvider>
     </React.StrictMode>
 );
