97e4025184df8c03e2b9c4af632029f5337d4c37
sharmavins23 <sharmavins23@gmail.com>
2023-08-11 19:06:00 -0400

    Removed lib files as npm exists

--- src/Quadrants/BoardQuadrant/BoardQuadrant.tsx
+++ src/Quadrants/BoardQuadrant/BoardQuadrant.tsx
@@ -1,7 +1,14 @@
+import { Stack } from "@mui/material";
+import Chessboard from "./Chessboard/Chessboard";
+
 export default function BoardQuadrant() {
     return (
         <>
-            <h1>Board quadrant</h1>
+            <Stack spacing={2}>
+                <h1>Stockfish eval (TODO)</h1>
+                <Chessboard />
+                <h1>Buttons (TODO)</h1>
+            </Stack>
         </>
     );
 }
---
+++ src/Quadrants/BoardQuadrant/Chessboard/Chessboard.tsx
@@ -0,0 +1,8 @@
+export default function Chessboard() {
+    return (
+        <>
+            <div className="chessboard" style={{ width: "400px" }}></div>
+            <h1>Chessboard</h1>
+        </>
+    );
+}
